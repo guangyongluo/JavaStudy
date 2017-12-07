87296c294ad1628783e1cb081794cc9d60abb3e0
guangyongluo <llooww@mail.ustc.edu.cn>
2017-12-07 16:17:23 +0800

    Study(com.lwei.test): pull a trap case about java varargs.

--- src/main/java/com/lwei/test/TestVarargs.java
+++ src/main/java/com/lwei/test/TestVarargs.java
@@ -9,6 +9,12 @@ public class TestVarargs {
 		}
 		return sum;
 	}
+	
+	private static void m1(String s, String... ss) {
+        for (int i = 0; i < ss.length; i++) {
+            System.out.println(ss[i]);
+        }
+    }
 
 	public static void main(String[] args) {
 		int sum = 0;
@@ -16,6 +22,10 @@ public class TestVarargs {
 		System.out.println(sum);
 		sum = sum(1, 2, 3);
 		System.out.println(sum);
+		
+		m1("");
+        m1("aaa");
+        m1("aaa", "bbb");
 	}
 
 }
